dcf43f9f402c3dc43d44cdc8725c809ad51a293c
Garrett Brady <gab5654@psu.edu>
2025-07-07 11:32:33 -0400

    minimal working dose grid instantiation using spacial fft

--- main.jl
+++ main.jl
@@ -1,149 +1,74 @@
-using Distributions
 using DataFrames
-using SpecialFunctions
 using LinearAlgebra
+using AbstractFFTs
+using FFTW
+using PaddedViews
 using Plots
 
-function volumetric_gauss(σ::Union{Float64, Integer}, x::AbstractVector, y::AbstractVector)
+function gauss_2d(σ::Union{Float64, Integer}, center::Vector, position::Vector)
+    Δr_squared = sum((position .- center) .^2)
+    exponent = - 0.5 * Δr_squared / (σ^2)
+    return 1 / (2*pi*σ) * exp(exponent)
+end
+
+function gauss_matrix(σ::Union{Float64, Integer}, ΔX ::Union{Integer, Float64})
     #=
-    function which returns the 2d volumetric approximation of the multivariate gauss distribution
+    generates diagonal matrix from gaussian view window
     in:
-        σ - standard deviation of the gaussian approximation
-        x - beam position in resist
-        y - imaginary beam position
+        σ - standard deviation
+        ΔX - side length of grid cell
+    =#
+    partician = 3 * σ / ΔX
+    n = round(Int, partician)
+    n = n % 2 == 0 ? n : n - 1 # reduces the size by 1 if the grid is even to properly center the data
+
+    g_matrix = zeros(Float64, n,n)
+
+    for i in 1:n
+        for j in 1:n
+            g_matrix[i,j] = gauss_2d(σ, [(n+1)/2, (n+1)/2], [i, j])
+        end
+    end
+
+    correction = sum(g_matrix)
+    g_matrix = g_matrix ./ correction
+
+    return g_matrix
+end
+
+function instantiate_dose_grid(θ::Union{Float64, Integer}, n::Integer, )
+    #=
+    in:
+        θ - energy ratio coefficient
+        n - array size
     out:
-        gauss approximation
+        D1 dose grid
     =#
 
-    ΔX = abs(x[1] - y[1])
-    ΔX_squared = ΔX^2
-
-    ΔY = abs(x[2] - y[2])
-    ΔY_squared = ΔY^2
-
-    r_squared = ΔX_squared + ΔY_squared
-    r = sqrt(r_squared)
-
-    if r <= (3 * σ)
-        A = 1 / (sqrt(2*pi) * σ) 
-        scatter = A * ℯ ^ (- r_squared / (2*σ^2))
-        volumetric = scatter * 2 * pi * r * ΔX * ΔY
-        return volumetric
-    end
-
-    return 0.0 
-end
-
-function summed_gauss(σ::Union{Float64, Integer}, x::AbstractVector, etched_grid::Matrix)
-    #=
-    in:
-        σ - standard deviation of gaussian approximation
-        x - beam position in resist
-        grid - bitmap which masks the etched and non etched areas, assumes grid is populated
-    =#
-    if σ <= 0
-        println("Invalid sigma value")
-        return nothing 
-    end
-
-    x_len, y_len = size(etched_grid)
-    t = 0
-
-    for xi in 1:x_len
-        for yi in 1:y_len
-            if etched_grid[xi, yi] > 0
-                s = volumetric_gauss(σ, x, [xi, yi])
-                t += s
-            end
-        end
-    end
-    return t
-end
-
-function instantiate_proximity_dose_grid(σ::Union{Float64, Integer}, η::Union{Float64, Integer}, etched_grid::Matrix)
-    #=
-    Creates the base case dosing grid
-    =#
-    n, m = size(etched_grid)
-    prox_dose_grid = zeros(Float64, n, m)
-
-    for xi in 1:n
-        for yi in 1:m
-            prox_dose_grid[xi, yi] = (.5 + η) / (.5 + η * summed_gauss(σ, [xi, yi], etched_grid))
-        end
-    end
-
-    return prox_dose_grid
-end
-
-function iterate_dose_grid_element(σ::Union{Float64, Integer}, η::Union{Float64, Integer}, x::AbstractVector, initial_dose_grid::Matrix, dose_grid::Matrix)
-
-    #=
-    Iterates an element of the dosing grid 
-    =#
-    n, m = size(initial_dose_grid)
-    t = 0
-
-    for xi in 1:n
-        for yi in 1:m
-            s = dose_grid[xi, yi] * volumetric_gauss(σ, x, [xi, yi])
-            t += s
-        end
-    end
-
-    ϵ = (1 / (0.5 + η)) * (( .5 * dose_grid[x[1], x[2]]) + η * t ) - 1
-    d = - ϵ * initial_dose_grid[x[1], x[2]]
-    D = dose_grid[x[1], x[2]] + d
-
-    return D
-end
-
-function iterate_dose_grid(σ::Union{Float64, Integer}, η::Union{Float64, Integer}, initial_dose_grid::Matrix, dose_grid::Matrix)
-    #=
-    Iterates the entire dosing grid
-    =#
-    n, m = size(initial_dose_grid)
-    new_dose_grid = zeros(Float64, n, m)
-
-    for xi in 1:n
-        for yi in 1:m
-            new_dose_grid[xi, yi] = iterate_dose_grid_element(σ, η, [xi, yi], initial_dose_grid, dose_grid)
-        end
-    end
-
-    return new_dose_grid
-end
-
-function resolve_dose_grid(σ::Union{Float64, Integer}, η::Union{Float64, Integer}, res::Integer, etched_grid::Matrix)
-    #=
-    Insert Docstring here
-    =#
-    D1_grid = instantiate_proximity_dose_grid(σ, η, etched_grid)
-
-    dose_grid = iterate_dose_grid(σ, η, D1_grid, D1_grid)
-
-    for i in 1:res
-        if (i+1) % 5 == 1 
-            println("Iteration $i / $res")
-        end
-
-        s = iterate_dose_grid(σ, η, D1_grid, dose_grid)
-        dose_grid = s
-    end
-
-    return dose_grid
 end
 
 function main()
-    global primary_resolution = 100
-    global secondary_resulution = 10 # temporary values to give physical meaning later
+    test_σ_back = 10
+    test_ΔX = 1
 
-    n = 100
-    test_matrix = Matrix{Int}(I, n, n) # using itentity matrix as test matrix
-    converged_dose = resolve_dose_grid(10, .80, 10, test_matrix)
+    n = round(Int, (3*test_σ_back / test_ΔX))
+    n = n % 2 == 0 ? n : n - 1 # reduces the size by 1 if the grid is even to properly center the data
+    pad = nextpow(2, n) # calculating the next power of 2 to speed up FFT
 
-    heatmap_1 = heatmap(converged_dose)
-    savefig(heatmap_1, "figures/test_heatmap")
+    dl_index = round(Int, (pad-n) / 2 ) + 1
+    dr_index = dl_index + n - 1
+
+    g = gauss_matrix(test_σ_back, test_ΔX)
+    mask = ones(Float64, n,n)
+
+    pad_mask = collect(PaddedView(0, mask, (1:pad, 1:pad), (dl_index:dr_index, dl_index:dr_index)))
+    pad_g = collect(PaddedView(0, g, (1:pad, 1:pad), (dl_index:dr_index, dl_index:dr_index)))
+
+    patterned_g = real.(ifft(fft(pad_mask) .* fft(pad_g)))
+    
+    p1 = heatmap(patterned_g)
+    savefig(p1, "figures/fft_heatmap.png")
+
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
---
+++ test.jl
@@ -0,0 +1,13 @@
+using LinearAlgebra
+using AbstractFFTs
+using FFTW
+using Plots
+
+function main()
+    shkeebert = missing
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    results = main()
+end
