40f097e3026fc7120bf0bf5dd6435394aa8c51e2
Garrett Brady <gab5654@psu.edu>
2025-07-23 14:57:44 -0400

    Working backscattering correction utilizing FFT convolutions

--- main.jl
+++ main.jl
@@ -48,8 +48,41 @@ function grid_padder(a::Matrix, pad::Integer, value::Union{Float64, Integer} = 0
     return collect(PaddedView(value, a, (1:pad, 1:pad), (lower:upper, lower:upper)))
 end
 
+function dose_grid_step(lower::Integer, upper::Integer, η::Union{Float64, Integer}, dose_grid::Matrix, kernel::Matrix, modified_pattern::Matrix)
+    dose_grid_roi = dose_grid[lower:upper, lower:upper]
+    
+    conv = real.((ifft(fft(dose_grid) .* fft(kernel))))
+    conv_roi = conv[lower:upper, lower:upper] # assume kernel is already ifft shfited
+
+    A = 1 / (.5 + η)
+    ϵ = zeros(Float64, size(modified_pattern))
+    ϵ = A .* (dose_grid_roi ./ 2 .+ η .* conv_roi) .- 1
+    dose_correction = .- ϵ .* modified_pattern .+ dose_grid_roi
+    
+    return dose_correction
+end
+
+function converge_dose_grid(pad, iter, lower, upper, η, dose_grid, kernel, modified_pattern)
+    corner_sample = []
+    center_sample = []
+
+    n, _ = size(modified_pattern)
+    center = Int(floor(n/2) + 1)
+    
+    for i in 1:iter
+        println("Iterating step $i / $iter")
+        padded_dose = grid_padder(dose_grid, pad)
+        dose_grid = dose_grid_step(lower, upper, η, padded_dose, kernel, modified_pattern)
+
+        push!(corner_sample, dose_grid[2,2])
+        push!(center_sample, dose_grid[center,center])
+    end
+
+    return dose_grid, corner_sample, center_sample
+end
+
 function main()
-    η = .1
+    η = .2
     n = 1000 # microns (rescaling this number rescales the gridspec)
     test_σb = 30 # microns
 
@@ -68,26 +101,24 @@ function main()
     println("Padding $n to $pad")
 
     pad_mask = grid_padder(pattern_mask, pad)
-    pad_g = grid_padder(g_matrix, pad)
+    pad_g = ifftshift(grid_padder(g_matrix, pad))
 
-    # display(pad_g)
-
-    patterned_g = real.((ifft(fft(pad_mask) .* fft(ifftshift(pad_g)))))
-
+    patterned_g = real.((ifft(fft(pad_mask) .* fft(pad_g))))
+    
     lower = round(Int, (pad-n) / 2) + 1
     upper = lower + n - 1
+    
+    initial_dose = zeros(Float64, n, n)
+    initial_dose = (.5 .+ η) ./ (.5 .+ η .* (patterned_g[lower:upper, lower:upper]))
 
-    # dose_grid_it = converge_dose_grid(pad, lower, upper, η, padded_dose_grid, pad_g, dose_grid)
-    #=
-    println("Padded Mask:")
-    display(pad_mask)
-    println("Pad G:")
-    display(pad_g)
-    println("FFT Convolution output:")
-    display(patterned_g)
-    =#
-    p1 = heatmap(patterned_g[lower:upper, lower:upper])
+    test, corner_sample, center_sample = converge_dose_grid(pad, 20, lower, upper, η, grid_padder(initial_dose, pad), pad_g, initial_dose)
+
+    p1 = heatmap(test)
+    p2 = plot(corner_sample)
+    p3 = plot(center_sample)
     savefig(p1, "figures/fft_ifftshifted_heatmap.png")
+    savefig(p2, "figures/corner_sample.png")
+    savefig(p3, "figures/center_sample.png")
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
